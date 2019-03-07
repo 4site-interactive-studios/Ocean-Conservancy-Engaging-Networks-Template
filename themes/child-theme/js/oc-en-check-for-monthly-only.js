d72bf777a20ae956dccfb88277484c8426d10043
bryancasler <bryan.casler@gmail.com>
2019-03-07 16:53:20 -0500

    Only show Give by Check when giving Monthly
    REF: https://app.productive.io/2650-4site-interactive-studios-inc/m/task/282904

---
+++ themes/child-theme/js/oc-en-check-for-monthly-only.js
@@ -0,0 +1,32 @@
+(function() {  
+window.addEventListener('load', function() {
+
+  var payment_by_check_block_class_name = 'give-by-check';
+  var payment_by_check_input_id = 'en__field_supporter_questions_1801652';
+  var payment_by_card_input_id = 'en__field_supporter_questions_1801650';
+
+  function updateCheckPaymentDisplay() {
+    // check if this is monthly or one-time ... if monthly, show check option if it exists on page, otherwise hide check option
+    var frequency = getDonationFrequency();
+    var check_payment_method = document.getElementById(payment_by_check_input_id);
+    if(frequency.indexOf('onth') !== -1) { // checking for "month", which may be capitalized
+      check_payment_method.parentElement.style.display = '';
+    } else {
+      if(getPaymentType() == 'check') {
+        document.getElementById(payment_by_card_input_id).click();      
+      }
+      check_payment_method.parentElement.style.display = 'none';
+    }
+  }
+
+  // Add our donation frequency change handler
+  var donation_frequency_buttons = document.querySelectorAll('input[name="' + payment_frequency_name + '"]');
+  for(i = 0; i < donation_frequency_buttons.length; i++) {
+    donation_frequency_buttons[i].addEventListener('change', function() {
+      updateCheckPaymentDisplay();
+    });
+  }
+
+  updateCheckPaymentDisplay();
+});
+})();
