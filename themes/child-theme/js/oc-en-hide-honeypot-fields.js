fd3c625fd1f865b6a8aacb2b90194b6eeb613442
Michael Wilson <michaelw@4sitestudios.com>
2019-03-11 01:49:49 +0000

    conditional test for valid element

--- themes/child-theme/js/oc-en-hide-honeypot-fields.js
+++ themes/child-theme/js/oc-en-hide-honeypot-fields.js
@@ -9,9 +9,11 @@ window.addEventListener('load', function() {
   // hide the fields, make them un-tabbable, turn off autocomplete
   honey_pot_fields.forEach(function(hp_class) {
     var hp_field = document.querySelector(hp_class);
-    hp_field.style.display = 'none';
-    hp_field.tabIndex = -1;
-    hp_field.autocomplete = 'off';
+    if(hp_field) {
+      hp_field.style.display = 'none';
+      hp_field.tabIndex = -1;
+      hp_field.autocomplete = 'off';      
+    }
   });
 });
 })();
